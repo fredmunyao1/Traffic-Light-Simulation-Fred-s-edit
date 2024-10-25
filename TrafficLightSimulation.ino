570037c42297306c9fb343f5f8880ae54a2b1dde
Fred Muyo <105910861+fredmunyao1@users.noreply.github.com>
2024-10-25 09:38:34 +0300

    Updated the TrafficLightSimulation.ino by adding all logics needed - FRED
    Made it work

--- TrafficLightSimulation.ino
+++ TrafficLightSimulation.ino
@@ -1,4 +1,4 @@
-// LED Traffic Light Simulation
+ // LED Traffic Light Simulation
 // This program simulates a traffic light system with a pedestrian crossing button.
 
 const int redLightPin = 23;   // Red LED connected to GPIO 23
@@ -7,15 +7,50 @@ const int greenLightPin = 21;  // Green LED connected to GPIO 21
 const int buttonPin = 19;      // Pedestrian crossing button connected to GPIO 19
 
 void setup() {
-  // Initialize the LED pins as outputs
-  // TODO: Set the button pin as INPUT
-}
+  pinMode(redLightPin, OUTPUT);
+  pinMode(yellowLightPin, OUTPUT);
+  pinMode(greenLightPin,OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  }
 
 void loop() {
-  // TODO: Implement the traffic light sequence
-  // Use delays for light timing: Red, Yellow, Green
-  
+  if(buttonPin == LOW){
+    Pedestrian();
+  }
+  else{
+    Go();
+    delay(5000);
+    Wait();
+    delay(1000);
+    Stop();
+    delay(3000);    
+  }
   // TODO: Implement pedestrian crossing logic
   // Change light sequence when button is pressed
 }
+void Stop(){
+  digitalWrite(redLightPin, HIGH);
+  digitalWrite(yellowLightPin, LOW);
+  digitalWrite(greenLightPin, LOW);
+}
 
+void Wait(){  
+  digitalWrite(redLightPin, LOW);
+  digitalWrite(yellowLightPin, HIGH);
+  digitalWrite(greenLightPin, LOW);
+}
+
+void Go(){  
+  digitalWrite(redLightPin, LOW);
+  digitalWrite(yellowLightPin, LOW);
+  digitalWrite(greenLightPin, HIGH);
+}
+
+void Pedestrian(){ 
+  for (int i = 0; i<15; i++){ 
+  digitalWrite(greenLightPin, HIGH);
+  delay(500);
+  digitalWrite(greenLightPin, LOW);
+  delay(500);
+  }
+}
